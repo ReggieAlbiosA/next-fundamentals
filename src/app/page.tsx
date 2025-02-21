a4533e2d089304f725cb17cea469851ddbbce015
ReggieAlbiosA <pureprogrammingph@gmail.com>
2025-02-21 18:48:09 +0800

    created html tag google-site-verification from metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@ import type { Metadata } from 'next'
 export const metadata: Metadata = {
   title: 'Home Page',
   description: 'Welcome to my home page',
+  verification: {
+    google: "L1mPIjja_otzsFGOpSNb7WoMPmne53qIRLdWph-iJFA",
+  }
 }
 
 export default function Home() {
