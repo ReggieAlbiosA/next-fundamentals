0f54c2e1ef8118eb67b1d5a59aabf6bf7cc41fa4
ReggieAlbiosA <pureprogrammingph@gmail.com>
2025-02-22 01:05:29 +0800

    style changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import "@/styles/globals.css";
 import { Geist, Geist_Mono } from "next/font/google";
 import Link from "next/link";
+import Image from 'next/image';
 import { Analytics } from "@vercel/analytics/react"
+import BrandLogo from '@/images/brand-logo.svg'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,14 +23,31 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <header className="min-h-[68px] flex items-center justify-center shadow-[0px_1px_4px_rgba(0,0,0,0.25)] sticky top-0 z-50">
-          <nav>
-            <ul className="flex gap-x-[20px]">
-              <li><Link href="/">Home</Link></li>
-              <li><Link href="/blogpost">Blog Post</Link></li>
-            </ul>
-          </nav>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased body-bg-theme`} >
+        <header className="header-bg-theme min-h-[68px] flex items-center justify-center shadow-[0px_1px_4px_rgba(0,0,0,0.25)] sticky top-0 z-50">
+          <div className="w-full max-w-[1200px] flex justify-between items-center">
+            <div className="flex gap-x-[20px]">
+                <Link href="/" id="brand-logo">
+                    <Image
+                      src={BrandLogo} // path relative to the public folder
+                      alt="Your Brand Logo"
+                      width={35}
+                      height={35}
+                    />
+                </Link>
+              
+              <nav id="main-nav" className="flex gap-x-[20px]">
+                <ul className="flex gap-x-[20px] items-center">
+                  <li className="opacity-100 hover:opacity-90"><Link href="/blogpost">Blog Post</Link></li>
+                </ul>
+              </nav>
+            </div>
+            
+            <div className="flex items-center gap-x-[25px] " id="auth-actions">
+              <Link href="/signin" className="opacity-100 hover:opacity-90" id="sign-in">Sign In</Link>
+              <Link href="/signup" className="text-[#27ae60] border-[1px] border-[#27ae60] rounded-[5px] px-[15px] py-[5px] hover:bg-[#27ae60] hover:text-white" id="sign-up">Sign Up</Link>
+            </div>
+          </div>
         </header>
 
         <main className="flex flex-col items-center justify-center pt-6">{children}</main>
